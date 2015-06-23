96791490ca2cc38144f5b0ac104913811e1fe2de
stijnmoreels <stijn.moreels@student.howest.be>
2015-06-23 10:37:45 +0200

    Init

---
+++ nmct.own.extension.csv.test/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nmct.own.extension.csv.test
+{
+    public class Person
+    {
+        [CsvAttribute]
+        public string Name { get; set; }
+        [CsvAttribute]
+        public string Email { get; set; }
+        [CsvAttribute]
+        public string Address { get; set; }
+        [CsvAttribute]
+        public int Phone { get; set; }
+
+        public string Extra { get; set; }
+
+        public Person(string name, string email, string address, int phone)
+        {
+            this.Name = name;
+            this.Email = email;
+            this.Address = address;
+            this.Phone = phone;
+        }
+    }
+}
---
+++ nmct.own.extension.csv.test/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using nmct.own.extension.csv;
+using System.Diagnostics;
+
+namespace nmct.own.extension.csv.test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person stijn = new Person("stijn", "email-stijn", "grensstraat", 123456789);
+            Person lars = new Person("lars", "email-lars", "grensstraat", 123456789);
+            List<Person> persons = new List<Person>()
+            {
+                stijn, lars
+            };
+
+            string bigString = String.Empty;
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            Console.WriteLine("Start adding...");
+            for(int i = 0, l = 10000; i < l; i++)
+            {
+                bigString += persons.ToCsv<Person>();
+                Console.WriteLine("Big String adding: " + i);
+            }
+
+            watch.Stop();
+            Console.WriteLine("Stop adding...");
+        }
+    }
+}
